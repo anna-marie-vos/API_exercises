37f2a02a3bd25abb18e6bd0ba003bc58813fd185
anna-marie-vos <amvos@outlook.com>
2017-01-03 13:48:14 +1300

    final commit at 1:45

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', start);
 
 function start(){
-var address = 'Wellington, NZ';
-console.log(document.getElementById('map'))
-var map = new google.maps.Map(document.getElementById('map'), {
-    mapTypeId: google.maps.MapTypeId.TERRAIN,
-    zoom: 6
-});
+  var address = 'Wellington, NZ';
+  console.log(document.getElementById('map'))
+  var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      zoom: 6
+  });
 
-var geocoder = new google.maps.Geocoder();
+  var geocoder = new google.maps.Geocoder();
 
-geocoder.geocode({
-   'address': address
-},
-function(results, status) {
-   if(status == google.maps.GeocoderStatus.OK) {
-      new google.maps.Marker({
-         position: results[0].geometry.location,
-         map: map
-      });
-      map.setCenter(results[0].geometry.location);
-   }
-   else {
-      // Google couldn't geocode this request. Handle appropriately.
-   }
-});
+  geocoder.geocode({
+     'address': address
+  },
+  function(results, status) {
+     if(status == google.maps.GeocoderStatus.OK) {
+        new google.maps.Marker({
+           position: results[0].geometry.location,
+           map: map
+        });
+        map.setCenter(results[0].geometry.location);
+     }
+     else {
+        // Google couldn't geocode this request. Handle appropriately.
+     }
+  });
 }
