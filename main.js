14fb836cf4406a68125f5772165f788f0dbffbbe
anna-marie-vos <amvos@outlook.com>
2017-01-03 13:17:21 +1300

    test html changed to display map only

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var fs = require('fs')
 var routes = require('./routes')
 var app = express()
 var env = require('dotenv').config()
+var google = require('google')
 
 // view engine setup
 app.engine('handlebars', expresshbs({defaultLayout: 'main'})) // makes the main page html file work.
@@ -17,7 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 // stuff above are all setup type things
 
 var client = new google({
-  key: process.env.API_KEY
+  // key: process.env.API_KEY
+  key: process.env.GOOGLE_MAPS_JAVASCRIPT_API_KEY
 });
 
 var parameters = {}
@@ -27,7 +29,7 @@ client.get('search',parameters,function(error, xxx,response){
   if(error){
     console.log(error)
   }
-  
+
 } )
 
 app.get('/',routes.getIndex);
