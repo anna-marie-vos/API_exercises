24909a82cbcb84d8838cd2b4732c757311e95d9a
anna-marie-vos <amvos@outlook.com>
2017-01-03 18:39:15 +1300

    no change made - final commit

--- routes.js
+++ routes.js
@@ -5,9 +5,9 @@ var options = {
   provider: 'google',
 
   // Optional depending on the providers
-  httpAdapter: 'https', // Default
-  apiKey: process.env.GOOGLE_MAPS_JAVASCRIPT_API_KEY, // for Mapquest, OpenCage, Google Premier
-  formatter: null         // 'gpx', 'string', ...
+  httpAdapter: 'https',
+  apiKey: process.env.GOOGLE_MAPS_JAVASCRIPT_API_KEY,
+  formatter: null       
 };
 
 var geocoder = NodeGeocoder(options);
