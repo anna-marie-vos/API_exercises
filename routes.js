08507bc659a3e6ff3e177baf1acabef2169b4583
anna-marie-vos <amvos@outlook.com>
2017-01-03 17:51:10 +1300

    using node-geocoder to make a request to google

--- routes.js
+++ routes.js
@@ -1,10 +1,33 @@
 var db = require('./db')
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+  provider: 'google',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: process.env.GOOGLE_MAPS_JAVASCRIPT_API_KEY, // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+
+var geocoder = NodeGeocoder(options);
+
+// using Promise
+geocoder.geocode('Wellington, NZ')
+  .then(function(res) {
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 
 function getIndex (req, res) {
   var data = {
-    url:'http://maps.google.com/maps/api/js?sensor=false'
+    url:'http://maps.google.com/maps/api/js?sensor=false',
+    div: '<div>hello</div'
   }
+
     res.render('index',data)
 }
 
